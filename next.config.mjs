43f14faec3bd5bbb07ff8920cc7a00abd6877e89
Maze <innoworldgames@gmail.com>
2024-09-23 20:13:01 +0200

    Still fixing stupid service worker PWA issue

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,8 @@ const nextConfig = {
 const config = withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 })(nextConfig);
 
 export default config;
