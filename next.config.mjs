b34f603f3f80e88d85ffdde6ffda877b788e591e
Maze <innoworldgames@gmail.com>
2024-09-23 19:36:04 +0200

    Attempt another fix to make PWA work offline

--- next.config.mjs
+++ next.config.mjs
@@ -17,21 +17,9 @@ const nextConfig = {
   },
 };
 
-export default withPWA({
+const config = withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "offlineCache",
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
 })(nextConfig);
+
+export default config;
